4b61033b91c282a3aaec8eab6004b2027729baee
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-18 15:31:40 +0530

    Added:WelCome.java

---
+++ addressBook.java
@@ -0,0 +1,10 @@
+package addressBook;
+
+public class WelCome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to AddressBook");
+	}
+
+}
