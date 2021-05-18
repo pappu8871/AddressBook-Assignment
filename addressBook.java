78cf014eff5a8fedcd894f63f6352fa969f1ead8
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-18 15:42:11 +0530

    Added:class AddressBookUC2.java use console

--- addressBook.java
+++ addressBook.java
@@ -1,8 +1,11 @@
 package addressBook;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class AddressBookUc1 {
+public class AddressBookUC2 {
+	
 	private String firstName;
 	private String lastName;
 	private String address;
@@ -10,7 +13,21 @@ public class AddressBookUc1 {
 	private String state;
 	private String zipcode;
 	private String phonenumber;
-    private String email;
+	private String email;
+	
+
+	public AddressBookUC2 (String fn, String ln, String ad, String ct, String st, String zp, String ph, String em) {
+		// TODO Auto-generated constructor stub
+	
+	firstName = fn;
+	lastName= ln;
+	address = ad;
+	city = ct;
+	state = st;
+	zipcode = zp;
+	phonenumber = ph;
+	email = em;
+}
 
 
 	public String getFirstName() {
@@ -83,99 +100,82 @@ public String getCity() {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
-	public  AddressBookUc1 (String firstName, String lastName, String address, String city, String state,
-			String zipcode, String phonenumber, String email) {
-		
-		// TODO Auto-generated constructor stub
-	setFirstName(firstName);
-	setLastName(lastName);
-	setAddress(address);
-	setCity(city);
-	setState(state);
-	setZipcode(zipcode);
-	setPhonenumber(phonenumber);
-	setEmail(email);
-}
-	
-	
-//public static void main(String[] args) {
-//// TODO Auto-generated method stub
-//Scanner scn = new Scanner(System.in);
-//
-//System.out.println("Enter First Name");
-//String firstname = scn.next();
-//
-//System.out.println("Enter Last Name");
-//String lastname = scn.next();
-//
-//System.out.println("Enter Address");
-//String address = scn.next();
-//
-//System.out.println("Enter city");
-//String city = scn.next();
-//
-//System.out.println("Enter State");
-//String state = scn.next();
-//
-//System.out.println("Enter zipcode");
-//String zipcode = scn.next();
-//
-//System.out.println("Enter phone Number");
-//String phonenumber = scn.next();
-//
-//System.out.println("Enter Email Id");
-//String emailid = scn.next();
-//
-//
-//System.out.println("First Name"  +firstname);
-//System.out.println("Last Name"  +lastname);
-//System.out.println("Address"  +address);
-//System.out.println("City Name"  +city);
-//System.out.println("Statet Name"  +state);
-//System.out.println("ZipCode"  +zipcode);
-//System.out.println("Phone Number"  +phonenumber);
-//System.out.println("email id" +emailid);
-// 
-//}
 
+
+@SuppressWarnings("unchecked")
 public static void main(String[] args) {
-
-Scanner scn = new Scanner(System.in);	
-
-String[] array = new String[8]; 
-int i;
-String firstName, lastName, address, city,  state,zipcode,  phonenumber,  email; 
-AddressBookUc1 a = new AddressBookUc1 ("Pappu", "kumar", "Behat North", "Jhanjharpur", "Bihar", "847403", "8871312779","pappu@gmail.com");
-
-firstName = a.getFirstName();
-lastName = a.getLastName();
-address = a.getAddress();
-city = a.getCity();
-state = a.getState();
-zipcode = a.getZipcode();
-phonenumber = a.getPhonenumber();
-email = a.getEmail();
-		
-array [0] = firstName;
-array [1] = lastName;
-array [2] = address;
-array [3] = city;
-array [4] = state;
-array [5] = zipcode;
-array [6] = phonenumber;
-array [7] = email;
-
-System.out.println(" Person Detail in array:");
-System.out.println("------------------------");
-
-for ( i = 0; i < array.length; i++) 
-{
-	System.out.println(array[i]);
-}
-
-}
-}
 	
+	ArrayList<AddressBookUC2> addressBook = new ArrayList<AddressBookUC2>();
+	
+	Scanner console = new Scanner(System.in);
+	
+	
+	while(true)
+	{
+
+		System.out.println("1)Add a new contact");
+//		System.out.println("2)Display all contacts");
+//		System.out.println("3)Delete a contact");
+		System.out.println("-1) Quit");
+		System.out.print("Enter Your choice: ");
+		
+		Scanner scn = new Scanner(System.in);
+		
+		int choice = scn.nextInt();
+		scn.nextLine();
+		switch(choice)
+		
+		{
+		case 1:
+		
+			
+			System.out.println("Enter First Name");
+			String firstname = scn.next();
+			
+			System.out.println("Enter Last Name");
+			String lastname = scn.next();
+			
+			System.out.println("Enter Address");
+			String address = scn.next();
+			
+			System.out.println("Enter city");
+			String city = scn.next();
+			
+			System.out.println("Enter State");
+			String state = scn.next();
+			
+			System.out.println("Enter zipcode");
+			String zipcode = scn.next();
+			
+			System.out.println("Enter phone Number");
+		    String phonenumber = scn.next();
+			
+			System.out.println("Enter Email Id");
+			String email = scn.next();
+			
+			
+			System.out.println("First Name:"  +firstname);
+			System.out.println("Last Name:"  +lastname);
+			System.out.println("Address:"  +address);
+			
+			System.out.println("City Name:"  +city);
+			System.out.println("Statet Name:"  +state);
+			System.out.println("ZipCode:"  +zipcode);
+			System.out.println("Phone Number:"  +phonenumber);
+			System.out.println("email id" +email);
+			
+//			addressBook.add(new AddressBookUC4(firstname, lastname, address, city,
+//					state, zipcode, phonenumber, email));
+		
+	
+		}
+	}
+}
+}
+
+
+
+
+
+
 
